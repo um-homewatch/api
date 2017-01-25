0c0dc1fad4623fa0fdc087f6e03f9a7d3aab1cee
José Sousa <josesousa9000@gmail.com>
2017-01-25 15:20:58 +0000

    Setup testing framework

--- Gemfile
+++ Gemfile
@@ -4,8 +4,10 @@ ruby "2.3.1"
 gem "rails", "~> 5.0.0", ">= 5.0.0.1"
 
 gem "active_model_serializers", "~> 0.10.0"
+gem "bcrypt", "~> 3.1.7"
 gem "devise_token_auth"
 gem "foreman"
+gem "knock"
 gem "pg"
 gem "pry-rails"
 gem "pry-remote"
@@ -13,10 +15,11 @@ gem "puma", "~> 3.0"
 
 group :development, :test do
   gem "factory_girl_rails"
-  gem "faker"
+  gem "faker", "~> 1.7.0"
   gem "pry-byebug"
   gem "rspec-rails"
   gem "rubocop", require: false
+  gem "webmock"
 end
 
 group :development do
@@ -29,5 +32,6 @@ end
 
 group :test do
   gem "database_cleaner"
+  gem "shoulda-matchers", "~> 3.1"
   gem "simplecov", require: false
 end
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,42 @@
+require "active_model_serializers"
+
+module ControllerMacros
+  def authenticate(user = nil)
+    user ||= FactoryGirl.create(:user)
+    request.headers["Authorization"] = token_for(user)
+  end
+
+  def serialize_to_json(resource, serializer: nil)
+    serializer ||= serializer_for(resource)
+
+    serializer.new(resource).to_json
+  end
+
+  def serialize_as_json(resource, serializer: nil)
+    serializer ||= serializer_for(resource)
+
+    serializer.new(resource).as_json
+  end
+
+  def parsed_response
+    deep_symbolize JSON.parse(response.body)
+  end
+
+  private
+
+  def token_for(user)
+    Knock::AuthToken.new(payload: { sub: user.id }).token
+  end
+
+  def serializer_for(resource)
+    ActiveModel::Serializer.serializer_for(resource)
+  end
+
+  def deep_symbolize(json)
+    if json.is_a? Array
+      json.map(&:deep_symbolize_keys)
+    else
+      json.deep_symbolize_keys
+    end
+  end
+end
