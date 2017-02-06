c847c5ae8a39f19bea163dd7a5ea7d32e28efce5
José Sousa <josesousa9000@gmail.com>
2017-02-06 16:22:17 +0000

    Return user object on login
    * Return the user object on login
    
    * Change the route name to something more appropriate

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,2 +1,12 @@
 class UserTokenController < Knock::AuthTokenController
+  def create
+    render json: user_with_token, status: :created
+  end
+
+  private
+
+  def user_with_token
+    serialized_user = UserSerializer.new(entity).attributes
+    serialized_user.merge(jwt: auth_token.token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "user_token" => "user_token#create"
+  post "auth" => "user_token#create"
 
   resource :users, only: [:create]
 
