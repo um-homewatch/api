aa4c869af0772f161f50c2cfadde553bb2f96d1f
José Sousa <josesousa9000@gmail.com>
2017-04-14 22:45:14 +0100

    Interact with hub

--- app/models/things/light.rb
+++ app/models/things/light.rb
@@ -1,2 +1,20 @@
 class Things::Light < Thing
+  def status
+    HTTParty.get(uri,
+      headers: { "Content-Type" => "application/json" },
+      query: connection_info)
+  end
+
+  def send_status(status)
+    HTTParty.put(uri,
+      headers: { "Content-Type" => "application/json" },
+      query: connection_info,
+      body: status.to_json)
+  end
+
+  private
+
+  def uri
+    home.tunnel + "/lights"
+  end
 end
