45b709fdf2c8a153031eee93cb4432e82cb66900
José Sousa <josesousa9000@gmail.com>
2017-04-17 16:43:22 +0100

    Standardize

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -7,4 +7,27 @@ class Thing < ApplicationRecord
   def connection_info
     self[:connection_info]&.symbolize_keys
   end
+
+  def status
+    HTTParty.get(uri,
+      headers: { "Content-Type" => "application/json" },
+      query: connection_params)
+  end
+
+  def send_status(status)
+    HTTParty.put(uri,
+      headers: { "Content-Type" => "application/json" },
+      query: connection_params,
+      body: status.to_json)
+  end
+
+  protected
+
+  def connection_params
+    connection_info.merge(subType: subtype)
+  end
+
+  def uri
+    # ...
+  end
 end
