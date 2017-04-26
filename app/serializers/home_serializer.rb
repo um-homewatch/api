191180b2d473dae7aa1bc1301380f710cc90c8ec
José Sousa <josesousa9000@gmail.com>
2017-04-26 01:40:31 +0100

    Lint serializers

--- app/serializers/home_serializer.rb
+++ app/serializers/home_serializer.rb
@@ -1,5 +1,5 @@
 class HomeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :tunnel,  :location, :ip_address
+  attributes :id, :name, :tunnel, :location, :ip_address
 
   has_many :scenarios
 end
