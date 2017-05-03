d86f1aecc470c800fc513e59991399c7d8ff5d6e
José Sousa <josesousa9000@gmail.com>
2017-05-03 16:05:21 +0100

    Fix things params

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -36,7 +36,7 @@ class ThingsController < ApplicationController
   private
 
   def thing_params
-    thing_params = params.require(:thing).permit(:id, :home_id, :subtype)
+    thing_params = params.require(:thing).permit(:subtype)
     thing_params[:connection_info] = params[:thing][:connection_info]
     thing_params.permit!
   end
