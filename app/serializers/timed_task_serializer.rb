71805a05488615a9098c6c117e43f719a19bbb6f
José Sousa <josesousa9000@gmail.com>
2017-06-21 18:56:57 +0100

    Display next_run on timed_tasks

--- app/serializers/timed_task_serializer.rb
+++ app/serializers/timed_task_serializer.rb
@@ -1,5 +1,10 @@
 class TimedTaskSerializer < OmmitNilSerializer
-  attributes :id, :status, :cron
+  attributes :id, :status, :cron, :next_run
+
   has_one :thing
   has_one :scenario
+
+  def next_run
+    object.delayed_job.run_at
+  end
 end
