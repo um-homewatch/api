145b745b18337e4a305dd49c393ff27c5b3e1d9c
José Sousa <josesousa9000@gmail.com>
2017-06-30 15:27:50 +0100

    Endpoint to refresh token

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,9 +1,28 @@
 class UserTokenController < Knock::AuthTokenController
+  include Knock::Authenticable
+  before_action :authenticate_user, only: [:show]
+  before_action :authenticate, only: [:create]
+
+  def show
+    render json: serialize_with_jwt(current_user)
+  end
+
   def create
     render json: user_with_token, status: :created
   end
 
   private
+
+  def serialize_with_jwt(user)
+    UserSerializer.
+      new(user).
+      as_json.
+      merge(jwt: token_for(user))
+  end
+
+  def token_for(user)
+    Knock::AuthToken.new(payload: { sub: user.id }).token
+  end
 
   def user_with_token
     serialized_user = UserSerializer.new(entity).attributes
--- spec/controllers/user_token_controller_spec.rb
+++ spec/controllers/user_token_controller_spec.rb
@@ -1,8 +1,21 @@
 require "rails_helper"
 
 describe UserTokenController, type: :controller do
-  context "POST #create" do
+  context "GET #show" do
     it "returns the user and a token" do
+      user = create(:user)
+
+      authenticate(user)
+      get :show
+
+      expect(parsed_response[:name]).to eq(user.name)
+      expect(parsed_response[:email]).to eq(user.email)
+      expect(parsed_response[:jwt]).to be
+    end
+  end
+
+  context "POST #create" do
+    it "creates a token given email and password" do
       user = create(:user)
       auth_params = { email: user.email, password: user.password }
 
