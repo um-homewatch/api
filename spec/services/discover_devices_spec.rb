a89aa3ba3b0b6a933f723f5c621350e2761a5b1e
José Sousa <josesousa9000@gmail.com>
2017-07-16 13:05:57 +0100

    Fix discovery

--- spec/services/discover_devices_spec.rb
+++ spec/services/discover_devices_spec.rb
@@ -4,43 +4,39 @@ describe DiscoverDevices do
   let(:home) { create(:home) }
 
   def build_uri(home, type)
-    service = DiscoverDevices.new(home: home, params: { type: type })
+    service = DiscoverDevices.new(home: home, params: { type: type, port: 1234 })
 
     service.send(:make_uri)
-  end
-
-  describe "type validation" do
-    it "should build uri with the type LIGHT" do
-      expect(build_uri(home, "Things::Light")).to be_truthy
-    end
-
-    it "should build uri with the type LOCK" do
-      expect(build_uri(home, "Things::Lock")).to be_truthy
-    end
-
-    it "should build a uri with the type THERMOSTAT" do
-      expect(build_uri(home, "Things::Thermostat")).to be_truthy
-    end
-
-    it "should build a uri with the type WEATHER" do
-      expect(build_uri(home, "Things::Weather")).to be_truthy
-    end
-
-    it "should fail with a unknown type" do
-      expect(build_uri(home, "meme")).to be_falsey
-    end
   end
 
   describe "#perform" do
     devices = [{ address: "192.168.1.200" }, { address: "192.168.1.150" }]
 
     it "should return a list of devices" do
-      service = DiscoverDevices.new(home: home, params: { type: "Things::Light", subtype: "hue" })
-      stub_discover!(home, "/lights/discover?type=Things::Light&subtype=hue", devices)
+      service = DiscoverDevices.new(home: home, params: { type: "Things::Light", subtype: "hue", port: "1234" })
+      stub_discover!(home, "/devices/lights/discover?subtype=hue&port=1234", devices)
 
       devices = service.perform
 
       expect(devices).to eq(devices)
+    end
+
+    it "should set status to true" do
+      service = DiscoverDevices.new(home: home, params: { type: "Things::Light", subtype: "hue", port: "1234" })
+      stub_discover!(home, "/devices/lights/discover?subtype=hue&port=1234", devices)
+
+      service.perform
+
+      expect(service.status).to be_truthy
+    end
+
+    it "should set status to false" do
+      service = DiscoverDevices.new(home: home, params: { type: "Things::NotImplemented", subtype: "hue", port: "1234" })
+      stub_discover!(home, "/devices/lights/discover?subtype=hue&port=1234", devices)
+
+      service.perform
+
+      expect(service.status).to be_falsy
     end
   end
 end
