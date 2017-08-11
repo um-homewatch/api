4961a0fa2c201d1d942c3679e2f6df8f54df1559
José Sousa <josesousa9000@gmail.com>
2017-08-11 01:44:25 +0100

    Fix migration inherit bug

--- db/schema.rb
+++ db/schema.rb
@@ -15,106 +15,106 @@ ActiveRecord::Schema.define(version: 20170802164202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
+  create_table "delayed_jobs", id: :serial, force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
+    t.string "locked_by"
+    t.string "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "cron"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+    t.string "cron"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.string   "name",           null: false
-    t.string   "location",       null: false
-    t.string   "tunnel",         null: false
-    t.inet     "ip_address",     null: false
-    t.integer  "user_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "token"
-    t.integer  "delayed_job_id"
-    t.index ["delayed_job_id"], name: "index_homes_on_delayed_job_id", using: :btree
-    t.index ["ip_address"], name: "index_homes_on_ip_address", unique: true, using: :btree
-    t.index ["user_id"], name: "index_homes_on_user_id", using: :btree
-  end
-
-  create_table "scenario_things", force: :cascade do |t|
-    t.integer  "thing_id"
-    t.integer  "scenario_id"
-    t.json     "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["scenario_id"], name: "index_scenario_things_on_scenario_id", using: :btree
-    t.index ["thing_id"], name: "index_scenario_things_on_thing_id", using: :btree
-  end
-
-  create_table "scenarios", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "home_id"
+  create_table "homes", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "location", null: false
+    t.string "tunnel", null: false
+    t.inet "ip_address", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["home_id"], name: "index_scenarios_on_home_id", using: :btree
+    t.string "token"
+    t.integer "delayed_job_id"
+    t.index ["delayed_job_id"], name: "index_homes_on_delayed_job_id"
+    t.index ["ip_address"], name: "index_homes_on_ip_address", unique: true
+    t.index ["user_id"], name: "index_homes_on_user_id"
   end
 
-  create_table "things", force: :cascade do |t|
-    t.string   "type"
-    t.string   "subtype"
-    t.json     "connection_info"
-    t.integer  "home_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "name"
-    t.index ["home_id"], name: "index_things_on_home_id", using: :btree
+  create_table "scenario_things", id: :serial, force: :cascade do |t|
+    t.integer "thing_id"
+    t.integer "scenario_id"
+    t.json "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["scenario_id"], name: "index_scenario_things_on_scenario_id"
+    t.index ["thing_id"], name: "index_scenario_things_on_thing_id"
   end
 
-  create_table "timed_tasks", force: :cascade do |t|
-    t.integer  "home_id"
-    t.integer  "delayed_job_id"
-    t.integer  "thing_id"
-    t.json     "status_to_apply"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "scenario_id"
-    t.index ["delayed_job_id"], name: "index_timed_tasks_on_delayed_job_id", using: :btree
-    t.index ["home_id"], name: "index_timed_tasks_on_home_id", using: :btree
-    t.index ["scenario_id"], name: "index_timed_tasks_on_scenario_id", using: :btree
-    t.index ["thing_id"], name: "index_timed_tasks_on_thing_id", using: :btree
+  create_table "scenarios", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "home_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["home_id"], name: "index_scenarios_on_home_id"
   end
 
-  create_table "triggered_tasks", force: :cascade do |t|
-    t.integer  "home_id",                            null: false
-    t.integer  "thing_id"
-    t.integer  "thing_to_compare_id",                null: false
-    t.integer  "scenario_id"
-    t.integer  "delayed_job_id"
-    t.json     "status_to_apply"
-    t.json     "status_to_compare",                  null: false
-    t.string   "comparator",                         null: false
-    t.boolean  "should_apply?",       default: true
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.index ["delayed_job_id"], name: "index_triggered_tasks_on_delayed_job_id", using: :btree
-    t.index ["home_id"], name: "index_triggered_tasks_on_home_id", using: :btree
-    t.index ["scenario_id"], name: "index_triggered_tasks_on_scenario_id", using: :btree
-    t.index ["thing_id"], name: "index_triggered_tasks_on_thing_id", using: :btree
-    t.index ["thing_to_compare_id"], name: "index_triggered_tasks_on_thing_to_compare_id", using: :btree
+  create_table "things", id: :serial, force: :cascade do |t|
+    t.string "type"
+    t.string "subtype"
+    t.json "connection_info"
+    t.integer "home_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["home_id"], name: "index_things_on_home_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  create_table "timed_tasks", id: :serial, force: :cascade do |t|
+    t.integer "home_id"
+    t.integer "delayed_job_id"
+    t.integer "thing_id"
+    t.json "status_to_apply"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "scenario_id"
+    t.index ["delayed_job_id"], name: "index_timed_tasks_on_delayed_job_id"
+    t.index ["home_id"], name: "index_timed_tasks_on_home_id"
+    t.index ["scenario_id"], name: "index_timed_tasks_on_scenario_id"
+    t.index ["thing_id"], name: "index_timed_tasks_on_thing_id"
+  end
+
+  create_table "triggered_tasks", id: :serial, force: :cascade do |t|
+    t.integer "home_id", null: false
+    t.integer "thing_id"
+    t.integer "thing_to_compare_id", null: false
+    t.integer "scenario_id"
+    t.integer "delayed_job_id"
+    t.json "status_to_apply"
+    t.json "status_to_compare", null: false
+    t.string "comparator", null: false
+    t.boolean "should_apply?", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["delayed_job_id"], name: "index_triggered_tasks_on_delayed_job_id"
+    t.index ["home_id"], name: "index_triggered_tasks_on_home_id"
+    t.index ["scenario_id"], name: "index_triggered_tasks_on_scenario_id"
+    t.index ["thing_id"], name: "index_triggered_tasks_on_thing_id"
+    t.index ["thing_to_compare_id"], name: "index_triggered_tasks_on_thing_to_compare_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "timed_tasks", "homes"
